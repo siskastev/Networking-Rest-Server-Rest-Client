198565d7754d5d0e2bb3525a7585a2291c07ea39
Siska Stevani <siska.stevani@gmail.com>
2018-11-21 14:03:38 -0800

    Networking commit

--- app/src/main/java/com/stevanigmail/siska/retrofitcllient/Rest/ApiClient.java
+++ app/src/main/java/com/stevanigmail/siska/retrofitcllient/Rest/ApiClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "http://192.168.1.81:8080/rest_server/";
+    public static final String BASE_URL = "http://192.168.43.245:8080/rest_server/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
